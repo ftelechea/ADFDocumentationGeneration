a69511bea80a8cf501157df2cbea323332e57ee5
Fernando Telechea <fernando.telechea@gmail.com>
2023-11-08 12:29:17 -0300

    Update read_pipelines.py
    Add mermaid diagram to the pipeline documentation file

--- read_pipelines.py
+++ read_pipelines.py
@@ -36,23 +36,93 @@ def read_pipeline(pipeline_file_name, markdown_file_name):
   Parameters
   ----------
   pipeline_file_name : str
-    The full path of the ADF json file to be read
+  The full path of the ADF json file to be read
   markdown_file_name : str
-    The full path of the markdown file to which the contents will be appended
+  The full path of the markdown file to which the contents will be appended
   """
 
-  pipelines_file = open(markdown_file_name, 'a')
+  # open /create markdown file
+  md = open(markdown_file_name, 'a')
   logging.info('\t reading %s' % (pipeline_file_name))
+
   with open(pipeline_file_name) as json_file:
     pipelines_data = json.load(json_file)
-    pipelines_file.write('\n\n ## %s \n' % pipelines_data['name'])
-    if 'description' in pipelines_data['properties']:
-      pipelines_file.write('\n Description: {0} \n'.format(pipelines_data['properties']['description']))
-    pipelines_file.write('\n\n ### Steps \n')
-    for act in pipelines_data['properties']['activities']:
-      pipelines_file.write('\n * Name: __{0}__, Type: {1} \n'.format(act['name'], act['type']))
+    md.write('\n\n ## %s \n' % pipelines_data['name'])
+    
+    # get properties section of the pipeline json file 
+    properties = pipelines_data.get('properties', {})
+
+    if 'description' in properties:
+      # get pipeline description
+      pipeline_description = properties['description']
+      
+      # write line in markdown file
+      md.write(f'\n Description: {pipeline_description} \n')
+      
+      md.write('\n\n ### Steps \n')
+    
+    # begin Mermaid code
+    mermaid_code = "```mermaid\ngraph LR;\n"
+    
+    # iterate over activities  
+    for act in properties['activities']:
+      # name and type activity
+      activity_name = act['name']
+      tipo_actividad = act['type']
+      md.write(f'\n - **Nombre:** {activity_name} \n')
+      md.write(f'\n   **Tipo:** {tipo_actividad} \n')
+      
+      # add activities to mermaid code
+      mermaid_code += f"{activity_name.replace(' ', '_')}({activity_name});\n"
+      
+      #  if exist activity description. add to markdown file 
+      if 'description' in act:
+        act_description = act['description']
+        md.write(f'\n   **Descripción:** {act_description} \n')
+        
+      # if exists dependencies add to markdown and mermaid code
       if len(act['dependsOn']) > 0:
-        pipelines_file.write('\n   Dependencies:')
+        md.write('\n   **Dependencias:**')
         for dep in act['dependsOn']:
-          pipelines_file.write('\n   * [{0}]({1}) ({2}) \n'.format(dep['activity'], '#'+dep['activity'].replace(' ', '-'), dep['dependencyConditions'][0]))
-  pipelines_file.close()
+          md.write('\n   * [{0}]({1}) ({2}) \n'.format(dep['activity'], '#'+dep['activity'].replace(' ', '-'), dep['dependencyConditions'][0]))
+          mermaid_code += f"{dep['activity'].replace(' ', '_')} --> {activity_name.replace(' ', '_')};\n"
+      
+      # If activities is of type Copy, extract SQL query of the source
+      if act['type'] == "Copy":
+        # write source info type
+        source_info = act['typeProperties']['source']
+        source_info_type = source_info['type']
+        md.write(f'\n   **Data Source Type:** {source_info_type} \n')
+        
+        # write sqlReaderQuery 
+        value_sql = source_info['sqlReaderQuery']['value']
+        md.write(f'\n   **SQL Statement:**  \n')
+        md.write(f'\n   ```SQL \n   {value_sql} \n   ```')
+        
+    # fin de actividades
+    mermaid_code +=  ' ```'
+    
+    # add mermaid diagram to markdown file
+    md.write('\n\n ### Activities Diagram \n')
+    md.write(f'\n {mermaid_code}')
+
+    # if exist parameter in pipeline, add to the parameters table in markdown
+    if 'parameters' in properties:
+      # Header Table
+      md.write('\n\n ### Parameters \n')
+      md.write('\n | Name | Type | Default Value |')
+      md.write('\n |---|---|---|')
+      
+      # get parameter section
+      parameters = properties.get('parameters', {})
+      # Iterate over each parameter
+      for parametro, info in parameters.items():
+          parameter_name = parametro
+          parameter_type = info.get('type')
+          default_value = info.get('defaultValue')
+          md.write(f'\n |{parameter_name}|{parameter_type}|{default_value}|')
+  
+  # close markdown file
+  md.close()
+  
+# end read_pipeline method
